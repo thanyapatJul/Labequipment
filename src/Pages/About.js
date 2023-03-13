6e429d1afc1c95ed5e8817c66bed5964f141f804
Khunanon Charoendee <109776878+KKhunanon@users.noreply.github.com>
2023-03-13 20:20:35 +0700

    add home,login,about us and register page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,13 +5,20 @@ import Someimage from '../img/Group 35.svg';
 // import { Typewriter } from 'react-simple-typewriter'
 // import Api from'../img/Api.png'
 // import Typ from'../img/Typ.png'
-
+import '../Styles/About.css'
 function About_page(){
 
 
     return (
-        <div className='About'>
-            <h1>About page</h1>
+        <div className='body-container'>
+            <div className='Container-About'>
+                <h1>About page</h1>
+                <div className='Block-Content'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu nunc euismod, dignissim enim ac, consectetur dolor. Nullam eget est id orci dignissim fringilla in sed lacus. Integer at nisl vel lectus tristique vehicula. Sed dignissim hendrerit lacus, ac rhoncus risus imperdiet eu. Suspendisse in elit in elit rhoncus elementum a eu arcu. Nullam dictum lobortis nulla, non aliquam enim venenatis sit amet. Morbi maximus tincidunt magna vel tincidunt. Duis eget nisl vitae odio convallis molestie id id nisl.</p>
+                    <p>Suspendisse convallis, nulla nec aliquet maximus, nibh metus mattis enim, ac interdum neque enim sit amet nibh. Nullam bibendum pellentesque lacus, eu fermentum magna lacinia ut. Aenean porttitor sapien sed dolor bibendum viverra. Proin convallis, magna eu sollicitudin venenatis, nisi sapien euismod nibh, non pulvinar enim tortor vitae dui. Sed fringilla ac lorem vel tristique. Etiam id lacus sit amet purus tincidunt eleifend. Vivamus vel magna urna. Etiam fringilla, metus a volutpat vestibulum, justo est malesuada ante, vitae tempor tellus mi in libero. </p>
+                    <p class="highlighted-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu nunc euismod, dignissim enim ac, consectetur dolor. Nullam eget est id orci dignissim fringilla in sed lacus.</p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import decoration from '../img/Group 37.svg';
+// import decoration from '../img/Group 37.svg';
 // import '../Styles/Home.css'
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import backgroundImage from '../img/KMUTNB_BG.jpg';
 
 
 function Home() {
   return (
     <div className='home'>
         <div className='headerContainer'>
-          <h1>Home page</h1>
+          {/* <h1>Home page</h1> */}
+          <div className='Bg-img'>
+              <img className="pic-mid" src={backgroundImage} alt ="bg"/>
+          </div>
         </div>
     </div>
   );
